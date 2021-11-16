b88ce6c11ca4934c9e499a9790a08cecd6ff6a87
susangle <susangle@uio.no>
2021-11-16 15:11:08 +0100

    Added comments

--- seq_lengths.py
+++ seq_lengths.py
@@ -1,12 +1,14 @@
 import sys
 
+#Prints lengths and header of all sequences
+
 with open(sys.argv[1], "r") as fh:
     lines = fh.readlines()
 
 for line in lines:
-    line = line.strip() #takes away newline at the end of all lines
-    if line.startswith(">"):
+    line = line.strip() #removes newline at the end of all lines
+    if line.startswith(">"): #header line starts with ">"
         header = line
-    else:
+    else: #all other are sequences
         length = len(line)
         print(length, header)
